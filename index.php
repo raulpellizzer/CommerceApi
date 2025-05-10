e160e3a15d4836cb0d0611382cdf097e91e84ac6
raul <raul1.pellizzer@gmail.com>
2025-05-10 08:15:17 -0300

    Add composer, .env

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php
-	echo "Hello, World!";
+	header("Access-Control-Allow-Origin: *");
+	header("Content-Type: application/json; charset=UTF-8");
+	header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+	header("Access-Control-Max-Age: 3600");
+	header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+	$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+	$uri = explode( '/', $uri );
+
+	echo "URI 1: $uri[1]\n";
+
+	// // all of our endpoints start with /person
+	// // everything else results in a 404 Not Found
+	// if ($uri[1] !== 'person') {
+	// 	header("HTTP/1.1 404 Not Found");
+	// 	exit();
+	// }
+
+	$requestMethod = $_SERVER["REQUEST_METHOD"];
+	echo "Request Method: $requestMethod\n";
+
+	require "bootstrap.php";
 ?>
