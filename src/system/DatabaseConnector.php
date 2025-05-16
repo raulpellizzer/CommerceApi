21a667853b608ae73464c5129081f219bd3e2520
raul <raul1.pellizzer@gmail.com>
2025-05-16 19:09:47 -0300

    Adjust comment

--- src/system/DatabaseConnector.php
+++ src/system/DatabaseConnector.php
@@ -34,9 +34,9 @@ class DatabaseConnector
      * Get the database connection
      *
      * Handles connection to the database between Api Auth DB and User DB.
-     * CommerceApiUsers db is responsible for authentication and user database name.
+     * CommerceApi db is responsible for authentication and user database name.
      * $database is the database name to connect to the user's database.
-     * First connection is to the auth database (CommerceApiUsers) to check user authentication. Once authenticated, the database name is retrieved for the given auth user.
+     * First connection is to the auth database (CommerceApi) to check user authentication. Once authenticated, the database name is retrieved for the given auth user.
      * Second connection ($this->dbConnection->getConnection(...)) ($database not null) is to the user's database (e.g. userModel->getUserDatabase($_SERVER['PHP_AUTH_USER'])) to perform operations.
      * 
      * @param string|null $database The database name to connect to
